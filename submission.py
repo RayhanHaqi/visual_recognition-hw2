b268119dad2e8443e646be5c3046c1b9ea777405
Rayhan <tilarayhan@gmail.com>
2026-04-06 12:48:42 +0800

    .

--- submission.py
+++ submission.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 # 1. KONFIGURASI
 # ==========================================
 TEST_IMG_DIR = "./datasets/test"           
-CHECKPOINT_PATH = "./checkpoints/rtdetr_v2_r50_best.pth"
+CHECKPOINT_PATH = "./checkpoints/rtdetr_r50_best.pth"
 
 # Direktori Output Baru
 SUBMISSION_DIR = "./submission"
